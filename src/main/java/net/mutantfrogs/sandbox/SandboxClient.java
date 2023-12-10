6f20f6b55ef6bb4d08a47a2aaedb023f38a8e2a1
mutantfrogs <themutantfrogs@gmail.com>
2023-12-09 19:08:36 -0500

    Initial commit

---
+++ src/main/java/net/mutantfrogs/sandbox/SandboxClient.java
@@ -0,0 +1,10 @@
+package net.mutantfrogs.sandbox;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SandboxClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/mutantfrogs/sandbox/SandboxDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mutantfrogs.sandbox;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SandboxDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
