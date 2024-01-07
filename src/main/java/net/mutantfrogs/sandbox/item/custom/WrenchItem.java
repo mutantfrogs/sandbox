6f57a0a3792a21a0c297cedfe4387c4f1ca1134e
mutantfrogs <themutantfrogs@gmail.com>
2024-01-07 14:58:12 -0500

    added wrench item functionality with slabs, hoppers, signs, player heads, and banners

--- src/main/java/net/mutantfrogs/sandbox/item/custom/WrenchItem.java
+++ src/main/java/net/mutantfrogs/sandbox/item/custom/WrenchItem.java
@@ -1,6 +1,8 @@
 package net.mutantfrogs.sandbox.item.custom;
 
 import net.minecraft.block.*;
+import net.minecraft.block.enums.SlabType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -14,7 +16,6 @@ import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
-import net.mutantfrogs.sandbox.Sandbox;
 
 import static net.minecraft.block.HorizontalFacingBlock.FACING;
 import static net.minecraft.state.property.Properties.AXIS;
@@ -26,46 +27,42 @@ public class WrenchItem extends Item {
     }
 
     @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        if (!world.isClient()) {
-            ItemStack itemStack = user.getStackInHand(hand);
-            return TypedActionResult.success(itemStack);
-        }
-        return super.use(world, user, hand);
-    }
-    @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
         BlockPos blockPos = context.getBlockPos();
         World world = context.getWorld();
         PlayerEntity user = context.getPlayer();
         BlockState blockState = context.getWorld().getBlockState(blockPos);
 
+            // rotates logs, pillars, hay bales, etc.
             if(blockState.contains(AXIS)){
                 Direction.Axis currentAxis = blockState.get(AXIS);
-                Direction.Axis rotatedAxis = flipAxis(currentAxis);
+                Direction.Axis rotatedAxis = rotateAxis(currentAxis);
 
                 BlockState newState = blockState.with(AXIS, rotatedAxis);
                 world.setBlockState(blockPos, newState, 3);
                 world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_FISHING_BOBBER_RETRIEVE, SoundCategory.PLAYERS, 1f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
 
-                Sandbox.LOGGER.info("Current Axis: " + currentAxis);
-
                 return ActionResult.SUCCESS;
             }
 
+            // rotates stairs, furnaces, anvils, terracotta, doors, buttons, etc.
             if (blockState.contains(FACING)) {
                 Direction currentFacing = blockState.get(FACING);
                 Direction rotatedFacing = rotateHorizontal(currentFacing);
+
+                /*// additional logic for flipping stairs
+                if(blockState.contains(Properties.BLOCK_HALF)){
+
+                }*/
 
                 BlockState newState = blockState.with(FACING, rotatedFacing);
                 world.setBlockState(blockPos, newState, 3);
                 world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_FISHING_BOBBER_RETRIEVE, SoundCategory.PLAYERS, 1f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
 
-                Sandbox.LOGGER.info("Current Facing: " + currentFacing);
-
                 return ActionResult.SUCCESS;
             }
 
+            // rotates observers, pistons, barrels, end rods, etc.
             if (blockState.contains(Properties.FACING)) {
                 Direction currentFacing = blockState.get(Properties.FACING);
                 Direction rotatedFacing = rotateAll(currentFacing);
@@ -74,7 +71,41 @@ public class WrenchItem extends Item {
                 world.setBlockState(blockPos, newState, 3);
                 world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_FISHING_BOBBER_RETRIEVE, SoundCategory.PLAYERS, 1f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
 
-                Sandbox.LOGGER.info("Current Facing: " + currentFacing);
+                return ActionResult.SUCCESS;
+            }
+
+            // rotates signs, banners, player heads
+            if (blockState.contains(Properties.ROTATION)) {
+                int currentFacing = blockState.get(Properties.ROTATION);
+                int rotatedFacing = rotateRotation(currentFacing);
+
+                BlockState newState = blockState.with(Properties.ROTATION, rotatedFacing);
+                world.setBlockState(blockPos, newState, 3);
+                world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_FISHING_BOBBER_RETRIEVE, SoundCategory.PLAYERS, 1f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
+
+                return ActionResult.SUCCESS;
+            }
+
+            // rotates slabs
+            if (blockState.contains(Properties.SLAB_TYPE)) {
+                SlabType currentType = blockState.get(Properties.SLAB_TYPE);
+                SlabType rotatedType = rotateSlab(currentType);
+
+                BlockState newState = blockState.with(Properties.SLAB_TYPE, rotatedType);
+                world.setBlockState(blockPos, newState, 3);
+                world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_FISHING_BOBBER_RETRIEVE, SoundCategory.PLAYERS, 1f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
+
+                return ActionResult.SUCCESS;
+            }
+
+            // rotates hoppers
+            if (blockState.contains(Properties.HOPPER_FACING)) {
+                Direction currentFacing = blockState.get(Properties.HOPPER_FACING);
+                Direction rotatedFacing = rotateHopper(currentFacing);
+
+                BlockState newState = blockState.with(Properties.HOPPER_FACING, rotatedFacing);
+                world.setBlockState(blockPos, newState, 3);
+                world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_FISHING_BOBBER_RETRIEVE, SoundCategory.PLAYERS, 1f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
 
                 return ActionResult.SUCCESS;
             }
@@ -82,6 +113,14 @@ public class WrenchItem extends Item {
         return super.useOnBlock(context);
     }
 
+ /*   public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand){
+
+        entity.kill();
+
+        return ActionResult.PASS;
+    }*/
+
+    // rotateHorizontal is for blocks using FACING for orientation
     private Direction rotateHorizontal(Direction direction) {
         switch (direction) {
             case NORTH: return Direction.EAST;
@@ -92,6 +131,7 @@ public class WrenchItem extends Item {
         }
     }
 
+    // rotateALL is for blocks using Properties.FACING for orientation
     private Direction rotateAll(Direction direction) {
         switch (direction) {
             case NORTH: return Direction.EAST;
@@ -104,12 +144,45 @@ public class WrenchItem extends Item {
         }
     }
 
-    private Direction.Axis flipAxis(Direction.Axis axis){
+    // rotateAxis is for blocks using Axis for orientation
+    private Direction.Axis rotateAxis(Direction.Axis axis){
         switch(axis){
             case X: return Direction.Axis.Y;
             case Y: return Direction.Axis.Z;
             case Z: return Direction.Axis.X;
             default: return axis;
+        }
+    }
+
+    // rotateRotation is for blocks using Axis for orientation
+    private int rotateRotation(int rotation){
+        rotation++;
+
+        if(rotation == 16){
+            rotation = 0;
+        }
+
+        return rotation;
+    }
+
+    // rotateHopper is for rotating hoppers
+    public Direction rotateHopper(Direction facing) {
+        switch(facing){
+            case NORTH: return Direction.EAST;
+            case EAST: return Direction.SOUTH;
+            case SOUTH: return Direction.WEST;
+            case WEST: return Direction.DOWN;
+            case DOWN: return Direction.NORTH;
+            default: return facing;
+        }
+    }
+
+    // rotateSlab is for orientating slabs
+    public SlabType rotateSlab(SlabType type) {
+        switch(type){
+            case TOP: return SlabType.BOTTOM;
+            case BOTTOM: return SlabType.TOP;
+            default: return type;
         }
     }
 }
