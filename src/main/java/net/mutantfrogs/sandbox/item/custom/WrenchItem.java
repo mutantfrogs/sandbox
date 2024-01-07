1b2f15147e532d40e7e017d0d2a56aba1f93e631
mutantfrogs <themutantfrogs@gmail.com>
2024-01-06 19:44:56 -0500

    fixed wrench item and updated texture

--- src/main/java/net/mutantfrogs/sandbox/item/custom/WrenchItem.java
+++ src/main/java/net/mutantfrogs/sandbox/item/custom/WrenchItem.java
@@ -1,17 +1,22 @@
 package net.mutantfrogs.sandbox.item.custom;
 
 import net.minecraft.block.*;
-import net.minecraft.client.render.WorldRenderer;
-import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.mutantfrogs.sandbox.Sandbox;
 
 import static net.minecraft.block.HorizontalFacingBlock.FACING;
+import static net.minecraft.state.property.Properties.AXIS;
 
 
 public class WrenchItem extends Item {
@@ -19,25 +24,60 @@ public class WrenchItem extends Item {
         super(settings);
     }
 
-
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        if (!world.isClient()) {
+            ItemStack itemStack = user.getStackInHand(hand);
+            return TypedActionResult.success(itemStack);
+        }
+        return super.use(world, user, hand);
+    }
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
         BlockPos blockPos = context.getBlockPos();
         World world = context.getWorld();
+        PlayerEntity user = context.getPlayer();
         BlockState blockState = context.getWorld().getBlockState(blockPos);
-        Direction direction = context.getSide();
-        Sandbox.LOGGER.info("Direction" + direction);
 
-        switch (direction){
-            case NORTH: direction = Direction.WEST; break;
-            case WEST: direction = Direction.SOUTH; break;
-            case SOUTH: direction = Direction.EAST; break;
-            case EAST: direction = Direction.NORTH; break;
-            case UP, DOWN: return super.useOnBlock(context);
-        }
+            if(blockState.contains(AXIS)){
+                Direction.Axis currentAxis = blockState.get(AXIS);
+                Direction.Axis rotatedAxis = flipAxis(currentAxis);
 
-        BlockState newState = blockState.with(FACING, direction);
-        world.setBlockState(blockPos, newState, 3);
+                BlockState newState = blockState.with(AXIS, rotatedAxis);
+                world.setBlockState(blockPos, newState, 3);
+                world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_FISHING_BOBBER_RETRIEVE, SoundCategory.PLAYERS, 1f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
+                return ActionResult.SUCCESS;
+            }
+
+            if (blockState.contains(FACING)) {
+                Direction currentFacing = blockState.get(FACING);
+                Direction rotatedFacing = rotateHorizontalClockwise(currentFacing);
+
+                BlockState newState = blockState.with(FACING, rotatedFacing);
+                world.setBlockState(blockPos, newState, 3);
+                world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_FISHING_BOBBER_RETRIEVE, SoundCategory.PLAYERS, 1f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
+                return ActionResult.SUCCESS;
+            }
+
         return super.useOnBlock(context);
+    }
+
+    private Direction rotateHorizontalClockwise(Direction direction) {
+        switch (direction) {
+            case NORTH: return Direction.EAST;
+            case EAST: return Direction.SOUTH;
+            case SOUTH: return Direction.WEST;
+            case WEST: return Direction.NORTH;
+            default: return direction;
+        }
+    }
+
+    private Direction.Axis flipAxis(Direction.Axis axis){
+        switch(axis){
+            case X: return Direction.Axis.Y;
+            case Y: return Direction.Axis.Z;
+            case Z: return Direction.Axis.X;
+            default: return axis;
+        }
     }
 }
