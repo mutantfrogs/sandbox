2f90e36b8983756a0027f484bdbc97d59b160f0f
mutantfrogs <themutantfrogs@gmail.com>
2024-01-07 15:04:46 -0500

    added wrench functionality with ROTATION, SLAB_TYPE, and HOPPER_FACING

--- src/main/java/net/mutantfrogs/sandbox/item/custom/WrenchItem.java
+++ src/main/java/net/mutantfrogs/sandbox/item/custom/WrenchItem.java
@@ -2,17 +2,13 @@ package net.mutantfrogs.sandbox.item.custom;
 
 import net.minecraft.block.*;
 import net.minecraft.block.enums.SlabType;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
